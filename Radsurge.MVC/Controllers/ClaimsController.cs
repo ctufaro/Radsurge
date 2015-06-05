a03d3170f1628d18ebc5b13c5dbdf35cca440457
unknown <ctufaro@VM-DEV-DEV3.mhtny.com>
2015-06-05 16:23:06 -0400

    Web API Controllers

--- Radsurge.MVC/Controllers/ClaimsController.cs
+++ Radsurge.MVC/Controllers/ClaimsController.cs
@@ -7,41 +7,35 @@ using System.Web.Http;
 
 namespace Radsurge.MVC.Controllers
 {
-    public class ValuesController : ApiController
+    public class ClaimsController : ApiController
     {
-        // GET api/values
+        // GET api/claims
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/claims/5
         public string Get(int id)
         {
             return "value";
         }
 
         [HttpPost]
-        public HttpResponseMessage Retrieve(string claimId, string merchantId)
+        public HttpResponseMessage Retrieve(string claim_id, string merchant_id)
         {
-            //http://localhost:50764/api/values/retrieve?claimId=5&merchantId=abcd
+            //http://localhost:50764/api/claims/retrieve?claim_id=a1b2c3&merchant_id=x4y5z6
             return Request.CreateResponse(HttpStatusCode.OK, "All Good");
         }
 
-        // PUT api/values/5
+        // PUT api/claims/5
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/claims/5
         public void Delete(int id)
         {
         }
-    }
-
-    public class Claim
-    {
-        public string ClaimId { get; set; }
-        public string MerchantId { get; set; }
     }
 }
