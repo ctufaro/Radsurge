41d2ee9dcd7acf5195f50d85a166ad287ccb1cbc
unknown <ctufaro@VM-DEV-DEV3.mhtny.com>
2015-06-24 15:28:15 -0400

    Continue Signup page

--- Radsurge.MVC/Controllers/RegisterController.cs
+++ Radsurge.MVC/Controllers/RegisterController.cs
@@ -16,5 +16,16 @@ namespace Radsurge.MVC.Controllers
             return View();
         }
 
+        public ActionResult SignUp(FormCollection form)
+        {
+            var role = form["role"].Equals("0") ? 0 : 1;
+            var username = form["username"];
+            var company = form["company"];
+            var email = form["email"];
+            var password = form["password"];
+            var password_confirm = form["password_confirm"];
+            return View("Index");
+        }
+
     }
 }
