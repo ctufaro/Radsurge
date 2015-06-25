0c0835cf2a29765d731e3dc2ffc27d599ba18cdd
unknown <ctufaro@VM-DEV-DEV3.mhtny.com>
2015-06-25 13:22:55 -0400

    testing cache

--- Radsurge.MVC/Models/RegisterModels.cs
+++ Radsurge.MVC/Models/RegisterModels.cs
@@ -25,7 +25,6 @@ namespace Radsurge.MVC.Models
         [Display(Name = "E-mail")]
         public string Email { get; set; }
 
-
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
